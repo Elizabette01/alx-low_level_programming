b7bd6e83cefcd52b1810490efc05df602ddcd6db
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-08 12:30:57 -0700

    writing a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-/* A C program that prints a particular line using the puts function*/
+/**
+ * main - prints the sring in the put function
+ * A C program that prints a particular line using the puts function
+ * Description: using the main function
+ * Return: 0
+ */
 
 #include <stdio.h>
 int main(void)
