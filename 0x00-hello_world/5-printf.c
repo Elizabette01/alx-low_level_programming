bfc18d289877086046a87605d116b0f08f908fff
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-08 12:46:44 -0700

    A C Program that displays an output using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ *  main - print the string in the fuction
+ *  Description: using the main fuction this program prints
+ *  with proper grammar, but the outcome is a piece of art
+ *  Return: (0)
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("wih proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
