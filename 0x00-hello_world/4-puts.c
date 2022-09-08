7e41df49ff47d0ef21eb6e8d737fdde09be8a876
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-08 12:23:20 -0700

    writing a c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* A C program that prints a particular line using the puts function*/
+
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
