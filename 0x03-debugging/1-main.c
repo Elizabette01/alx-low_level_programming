b064e0015ece2965a9685236688c4049f85907d5
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-14 16:15:05 -0700

    multiple mains

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,20 +4,24 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
-	1 = 0;
+	i = 0;
+
 	/*
-	 * while (i < 10)
-	 * {
-	 *	 putchar(i);
-	 * }
-	 */
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
+
 }
