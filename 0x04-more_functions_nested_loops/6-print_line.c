6f195dbafac274503ff610ad8a4e35343f3eada3
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-15 12:06:51 -0700

    A straigh line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,15 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i <= n; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
 		{
-			_putchar('_');
+			_putchar('_')
 		}
 		_putchar('\n');
 	}
