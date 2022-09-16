885b39397fa520858bfac532a21a7abc5cea8c43
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-15 18:50:28 -0700

    diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,21 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
+	i = 0;
+
+	while (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
+		j = i;
+		while (j > 0)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == 1)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
+			_putchar(' ');
+			j--;
 		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+		n--;
 	}
+	if (i < 1)
+		_putchar('\n');
 }
