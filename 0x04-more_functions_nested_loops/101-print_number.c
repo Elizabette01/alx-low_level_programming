15e7b5c4bb773e5a3f09917ec8e58dd05e6c4045
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-15 20:39:31 -0700

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,20 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
 
 	if (n < 0)
 	{
+		num = -n;
 		_putchar('-');
-		num = -num;
 	}
-	if (num > 9)
+	else
+	{
+		num = n;
+	}
+	if (num / 10)
 	{
 		print_number(num / 10);
 	}
-	putchar(num % 10 + '0');
+	putchar((num % 10) + '0');
 }
