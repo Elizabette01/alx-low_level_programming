553d19807c42cf9e0ec00e916b5d7deb2ea042e8
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-15 20:06:03 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,21 +5,27 @@
  **/
 void print_triangle(int size)
 {
-	int i, j;
+	int c, i, j;
 
-	if (size > 0)
+	c = 0;
+	i = size - 1;
+	while (c < size)
 	{
-		for (i = 1; i <= size; i++)
+		i = size - 1 - c;
+		j = c + 1;
+		while (i > 0)
 		{
-			for (j = i; j <= size; j++)
-			{
-				_putchar(' ');
-			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar(' ');
+			i--;
 		}
+		while (j > 0)
+		{
+			_putchar('#');
+			j--;
+		}
+		_putchar('\n');
+		c++;
 	}
+	if (size <= 0)
+		_putchar('\n');
 }
