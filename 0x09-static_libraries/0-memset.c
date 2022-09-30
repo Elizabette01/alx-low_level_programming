e4f019a64d4537bd2fc424104c2a862838ef0db6
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-30 15:44:35 -0700

    static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - a function that fills a memory with a constant byte
+ * @s: A pointer to the block of memory to fill
+ * @b: constant byte value o be set
+ * @n: size of byte to be set to the value
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
