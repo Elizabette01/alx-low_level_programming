8eb8c115944374e8fbbec8257968df909e2a4f3d
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-30 17:31:24 -0700

    Coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - minimum coins to make change for an amount of money
+ * @args: entry
+ * @argv: argument vector
+ * Return: 0 or 1 on error
+ */
+int main(int args, char *argv[])
+{
+	int i, coins = 0;
+
+	if (args != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+
+	if (i < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; i >= 0;)
+	{
+		if (i >= 25)
+			i -= 25;
+
+		else if (i >= 10)
+			i -= 10;
+
+		else if (i >= 5)
+			i -= 5;
+
+		else if (i >= 2)
+			i -= 2;
+
+		else if (i >= 1)
+			i -= 1;
+		else
+			break;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
