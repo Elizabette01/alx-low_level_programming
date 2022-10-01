86d18e6fc9c5a19ecce14e6ea971435ac5d07dc8
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-09-30 17:06:11 -0700

    add positive number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <type.h>
+#include <string.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int j;
+			char *str;
+
+			str = argv[i];
+			for (j = 0; str[j] != '\0'; j++)
+			{
+				if (str[j] < 48 || str[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
