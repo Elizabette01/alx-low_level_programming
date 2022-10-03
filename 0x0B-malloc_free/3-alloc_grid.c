a1abcf4b544b2b1b6e476f35383ab7a9f8408db5
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-03 06:47:03 -0700

    Alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,18 @@ int **alloc_grid(int width, int height)
 			free(p);
 			return (NULL);
 		}
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
+		}
+
 		for (a = 0; a < height; a++)
 		{
 			for (b = 0; b < width; b++)
