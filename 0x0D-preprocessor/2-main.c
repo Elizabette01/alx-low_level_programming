2288ad68636929a059a8611cd193cc3d17191834
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-10 13:54:01 -0700

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name of file it is compiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
