51a4d11047c0ea51f8621c276ee54a9e6ac0758e
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-12 14:21:06 -0700

    Basic calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,18 +13,18 @@ int (*get_op_func(char *s))(int, int)
 		 {"*", op_mul},
 		 {"/", op_div},
 		 {"%", op_mod},
-			 {NULL, NULL}
+		 {NULL, NULL}
 	 };
-	     int i;
+	 int i;
 
-	     i = 0;
-	     while (i < 5)
-	     {
-		     if (s[0] == ops[i].op[0])
-		     {
-			     return (ops[i].f);
-		     }
-		     i++;
-	     }
-	     return (NULL);
+	 i = 0;
+	 while (i < 5)
+	 {
+		 if (s[0] == ops[i].op[0])
+		 {
+			 return (ops[i].f);
+		 }
+		 i++;
+	 }
+	 return (NULL);
 }
