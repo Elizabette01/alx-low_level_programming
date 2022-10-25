bcd127b0895de2244b72022b4c94356666a03610
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-24 22:25:40 -0700

    9. insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,32 +2,18 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - insert a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: double pointer to the first node in the list
- * @idx: the idex of the list where the new node should be added
+ * @idx: index of the list where the new node should be inserted
  * @n: value for new node
  *
- * Return: Address of the new node or NULL if failed
+ * Return: address of the new node or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *current, *new;
+	listint_t *new, *current = *head;
 
-	if (head == NULL)
-		return (NULL);
-
-	if (idx != 0)
-	{
-		current = *head;
-		for (i = 0; i < idx - 1 && current != NULL; i++)
-		{
-			current = current->next;
-		}
-
-		if (current == NULL)
-			return (NULL);
-	}
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
@@ -36,10 +22,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *head;
+		new->next = current;
 		*head = new;
 		return (new);
 	}
-	new->next = new;
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (current == NULL || current->next == NULL)
+			return (NULL);
+		current = current->next;
+	}
+
+	new->next = current->next;
+	current->next = new;
+
 	return (new);
 }
