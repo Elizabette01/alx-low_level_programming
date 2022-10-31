535b73926ea9eaa7c0ff777f04d4ce9cbe863668
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-31 08:13:43 -0700

    1. Under the snow updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (o == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
@@ -26,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
-	if (w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
